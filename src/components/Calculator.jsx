6f2735c111dfef017d1c25a417b5c085bd18751b
Kathrine <kathrine.kodehode@gmail.com>
2023-01-26 14:02:37 +0100

    update

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -13,22 +13,24 @@ export default function Calculator(props) {
 
   return (
     <div className={CalculatorStyle.wrapper}>
-      <div className={CalculatorStyle.header}>
-        <span>0</span>
-        {0}
-      </div>
-      ;
-      <div className={CalculatorStyle.controls}>
-        <button>+</button>
-        <button>-</button>
-        <button>/</button>
-        <button>x</button>
-        <button>delete</button>
+      <div className={CalculatorStyle.calculator}>
+        <div className={CalculatorStyle.header}>
+          <span>0</span>
+          {0}
+        </div>
+        ;
+        <div className={CalculatorStyle.controls}>
+          <button>+</button>
+          <button>-</button>
+          <button>/</button>
+          <button>x</button>
+          <button>delete</button>
 
-        <div className={CalculatorStyle.numbers}>
-          <button>0</button>
-          <button>.</button>
-          <button>=</button>
+          <div className={CalculatorStyle.numbers}>
+            <button>0</button>
+            <button>.</button>
+            <button>=</button>
+          </div>
         </div>
       </div>
     </div>
